702397cb67c91f488aae9400bc26e030fd67f18f
3v1lb1t <agamotto65@protonmail.com>
2020-07-02 22:53:18 -0600

    added domain.js

---
+++ domain.js
@@ -0,0 +1,4 @@
+function newDomain() {
+	var ip = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
+	console.log(ip)
+}
