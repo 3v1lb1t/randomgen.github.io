0e771bc8a25001209a12f525804e16052e312d14
3v1lb1t <agamotto65@protonmail.com>
2020-07-02 23:01:34 -0600

    same as previous

--- domain.js
+++ domain.js
@@ -1,4 +1,4 @@
 function newDomain() {
 	var ip = (Math.floor(Math.random() * 255) + 1) + "." + (Math.floor(Math.random() * 255)) + "." + (Math.floor(Math.random() * 255)) + "." + (Math.floor(Math.random() * 255));
-	document.getElementById('domainDisplay').inner.HTML = ip;
+	document.getElementById('domainDisplay').inner.HTML = [ip];
 }
